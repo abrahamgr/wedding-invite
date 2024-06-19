e4d8df6c2eab7d001d5df5941d8ec46642525e3d
Abraham Galindo <abraham.garo@gmail.com>
2024-06-18 19:35:52 -0600

    chore: wording for pases

--- src/components/Confirmation.tsx
+++ src/components/Confirmation.tsx
@@ -9,9 +9,12 @@ interface ButtonProps extends PropsWithChildren {
 
 function Button({ children, type, invites }: ButtonProps) {
   const phoneNumber = type === 'novio' ? '8186936753' : '8123309117'
+  const isOne = invites === '1'
+  const wording = isOne ? 'Un lugar reservado' : `${invites} lugares reservados`
+
   return (
     <Link
-      href={`https://wa.me/52${phoneNumber}?text=${invites} lugares reservados - Boda Jaquelline y Abraham. Quiero confirmar la asistencia a la boda, anexo el nombre de las personas que asistirán para su registro, gracias`}
+      href={`https://wa.me/52${phoneNumber}?text=${wording} - Boda Jaquelline y Abraham. Quiero confirmar la asistencia a la boda, anexo el nombre de las personas que asistirán para su registro, gracias`}
       className='my-1 mr-2 rounded-md border-2 border-slate-300 p-1'
       target='_blank'
     >
@@ -25,6 +28,7 @@ interface ConfirmationProps {
 }
 
 export function Confirmation({ invites }: ConfirmationProps) {
+  const isOne = invites === '1'
   return (
     <div className='flex flex-col items-center bg-yellow-50 py-5 text-center *:px-2 md:text-xl'>
       <p className='text-xl md:text-2xl'>Sugerencia de regalo</p>
@@ -46,7 +50,9 @@ export function Confirmation({ invites }: ConfirmationProps) {
       />
       <br />
       <p>Agradecemos confirmar tu asistencia antes del 10 de Agosto</p>
-      <p className=''>Pase para {invites} personas</p>
+      <p className=''>
+        Pase para {isOne ? 'una persona' : `${invites} personas`}
+      </p>
       <div className='flex flex-col md:flex-row'>
         <Button type='novia' invites={invites}>
           Confirmar a la Novia
